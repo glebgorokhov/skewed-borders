47ee0f554e3c8200cf1ad0093fbe0ddb37530786
Gleb Gorokhov <theververy@gmail.com>
2023-02-21 15:54:03 +0300

    Add: generate a shape using SVG

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
+<template>
+  <div class="content" ref="content">Content</div>
+  <shape :el="content" class="shape" />
+</template>
+
 <script setup lang="ts">
 import Shape from "./components/Shape.vue";
+import {ref} from "vue";
 
-const width = 208;
+const content = ref();
 </script>
 
-<template>
-  <shape>
-    <div class="content">Content</div>
-  </shape>
-</template>
 
 <style lang="scss" scoped>
 .content {
-  padding: 18px 25px;
+  padding: 18px 24px;
+  background: rgba(white, .7);
+  color: black;
+  font-weight: 700;
+  clip-path: url(#mypath);
+  box-sizing: border-box;
+  backdrop-filter: blur(15px);
+}
+
+.shape {
+  position: fixed;
+  right: 100vw;
+  bottom: 100vh;
 }
 </style>
--- src/components/Shape.vue
+++ src/components/Shape.vue
@@ -1,82 +1,234 @@
 <template>
-  <div class="shape">
-    <div class="background">
-      <div class="side">
-        <div class="side-stretch left"></div>
-        <corner class="side-corner" :size="30" :radius="8" position="left bottom" />
-      </div>
-      <div class="middle"></div>
-      <div class="side">
-        <corner class="side-corner" :size="40" :radius="14" position="right top"/>
-        <div class="side-stretch right"></div>
-      </div>
-    </div>
-    <div class="content">
-      <slot />
-    </div>
-  </div>
+  <svg :viewBox="`0 0 ${width} ${height}`" xmlns="http://www.w3.org/2000/svg">
+    <clipPath :id="id">
+      <circle
+          v-for="(circle, circleKey) in circles"
+          :key="circleKey"
+          :cx="circle.x"
+          :cy="circle.y"
+          :r="circle.r"
+      />
+      <polygon :points="points" />
+    </clipPath>
+
+    <g v-if="svg">
+      <circle
+          v-for="(circle, circleKey) in circles"
+          :key="circleKey"
+          :cx="circle.x"
+          :cy="circle.y"
+          :r="circle.r"
+      />
+      <polygon :points="points" />
+    </g>
+  </svg>
 </template>
 
-<script lang="ts" setup>
-import Corner from "./Corner.vue";
+<script setup>
+import { computed, nextTick, onMounted, onUnmounted, ref } from "vue";
 
-const radius = 5;
+const props = defineProps({
+  width: {
+    type: Number,
+    default: 100,
+  },
+  height: {
+    type: Number,
+    default: 100,
+  },
+  svg: {
+    type: Boolean,
+  },
+  id: {
+    type: String,
+    default: "mypath",
+  },
+  el: {
+    type: Object,
+  },
+});
+
+const width = ref(100); // Default values
+const height = ref(50); // Default values
+
+const degreesToRadians = (degrees) => degrees * (Math.PI / 180);
+
+const getCathetus = (radius, cos = false) => {
+  const angle = degreesToRadians(45);
+  return radius * (cos ? Math.cos(angle) : Math.sin(angle));
+};
+
+let resizeObserver = null;
+const corners = [7, [7, 10], 7, [7, 15]];
+
+const circles = computed(() => {
+  const coordinates = [];
+
+  corners.forEach((corner, i) => {
+    if (Array.isArray(corner)) {
+      const [r, delta] = corner;
+
+      switch (i) {
+        case 0:
+          coordinates.push({
+            x: r,
+            y: r + delta,
+            r,
+            p1: [0, r + delta],
+            p2: [r - getCathetus(r, true), r + delta - getCathetus(r)],
+          });
+          coordinates.push({
+            x: r + delta,
+            y: r,
+            r,
+            p1: [r + delta - getCathetus(r, true), r - getCathetus(r)],
+            p2: [r + delta, 0],
+          });
+          break;
+        case 1:
+          coordinates.push({
+            x: width.value - r - delta,
+            y: r,
+            r,
+            p1: [width.value - r - delta, 0],
+            p2: [
+              width.value - r - delta + getCathetus(r, true),
+              r - getCathetus(r),
+            ],
+          });
+          coordinates.push({
+            x: width.value - r,
+            y: r + delta,
+            r,
+            p1: [
+              width.value - r + getCathetus(r, true),
+              r + delta - getCathetus(r),
+            ],
+            p2: [width.value, r + delta],
+          });
+          break;
+        case 2:
+          coordinates.push({
+            x: width.value - r,
+            y: height.value - r - delta,
+            r,
+            p1: [width.value, height.value - r - delta],
+            p2: [
+              width.value - r + getCathetus(r, true),
+              height.value - r - delta + getCathetus(r),
+            ],
+          });
+          coordinates.push({
+            x: width.value - r - delta,
+            y: height.value - r,
+            r,
+            p1: [
+              width.value - r - delta + getCathetus(r, true),
+              height.value - r + getCathetus(r),
+            ],
+            p2: [width.value - r - delta, height.value],
+          });
+          break;
+        case 3:
+        default:
+          coordinates.push({
+            x: r + delta,
+            y: height.value - r,
+            r,
+            p1: [r + delta, height.value],
+            p2: [
+              r + delta - getCathetus(r, true),
+              height.value - r + getCathetus(r),
+            ],
+          });
+          coordinates.push({
+            x: r,
+            y: height.value - r - delta,
+            r,
+            p1: [
+              r - getCathetus(r, true),
+              height.value - r - delta + getCathetus(r),
+            ],
+            p2: [0, height.value - r - delta],
+          });
+          break;
+      }
+
+      return;
+    }
+
+    const r = corner;
+
+    switch (i) {
+      case 0:
+        coordinates.push({
+          x: r,
+          y: r,
+          r,
+          p1: [0, r],
+          p2: [r, 0],
+        });
+        break;
+      case 1:
+        coordinates.push({
+          x: width.value - r,
+          y: r,
+          r,
+          p1: [width.value - r, 0],
+          p2: [width.value, r],
+        });
+        break;
+      case 2:
+        coordinates.push({
+          x: width.value - r,
+          y: height.value - r,
+          r,
+          p1: [width.value, height.value - r],
+          p2: [width.value - r, height.value],
+        });
+        break;
+      case 3:
+      default:
+        coordinates.push({
+          x: r,
+          y: height.value - r,
+          r,
+          p1: [r, height.value],
+          p2: [0, height.value - r],
+        });
+        break;
+    }
+  });
+
+  return coordinates;
+});
+
+const points = computed(() =>
+    circles.value.map((circle) => [circle.p1, circle.p2].join(",")).join(" ")
+);
+
+onMounted(() => {
+  nextTick(() => {
+    if (props.el) {
+      resizeObserver = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+          if (entry.contentBoxSize) {
+            const { inlineSize, blockSize } = entry.borderBoxSize[0];
+
+            width.value = inlineSize;
+            height.value = blockSize;
+          }
+        }
+      });
+
+      resizeObserver.observe(props.el);
+    }
+  });
+});
+
+onUnmounted(() => {
+  if (resizeObserver) {
+    resizeObserver.disconnect();
+  }
+});
 </script>
-
-<style lang="scss" scoped>
-.shape {
-  position: relative;
-  color: black;
-  font-weight: bold;
-}
-
-.background {
-  position: absolute;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-}
-
-.middle, .side-stretch, :deep(.square) {
-  background: white;
-}
-
-.middle {
-  flex: 1;
-}
-
-.content {
-  position: relative;
-}
-
-.side {
-  display: flex;
-  flex-direction: column;
-}
-
-.side-stretch {
-  flex-grow: 1;
-
-  &.left {
-    &:first-child {
-      border-top-left-radius: calc(v-bind(radius) * 1px);
-    }
-
-    &:last-child {
-      border-bottom-left-radius: calc(v-bind(radius) * 1px);
-    }
-  }
-
-  &.right {
-    &:first-child {
-      border-top-right-radius: calc(v-bind(radius) * 1px);
-    }
-
-    &:last-child {
-      border-bottom-right-radius: calc(v-bind(radius) * 1px);
-    }
-  }
-}
-</style>
