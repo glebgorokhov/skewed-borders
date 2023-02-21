acf9a41aefa5f5314fe8634225f23d16dc92d60c
Gleb Gorokhov <theververy@gmail.com>
2023-02-21 10:42:53 +0300

    Fix: deploy message

--- deploy.js
+++ deploy.js
@@ -3,6 +3,7 @@ import ghpages from "gh-pages";
 ghpages.publish('dist', {
   branch: "gh-pages",
   repo: "git@github.com:glebgorokhov/skewed-borders.git",
+  message: "Deploy: build to gh-pages"
 }, () => {
   console.log("Success!")
 });
